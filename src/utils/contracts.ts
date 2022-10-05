457f76c2a7931eda6051c051f0025a28fc5a56a9
fritzschoff <fritzschoff@icloud.com>
2022-10-05 11:44:27 -0500

    added correct address

--- src/utils/contracts.ts
+++ src/utils/contracts.ts
@@ -2,7 +2,6 @@ import { Contract } from 'ethers';
 import GnosisSafeL2 from '../abi/GnosisSafeL2.json';
 
 export const SafeContract = new Contract(
-  '0xd9B144b971aFbBd25668b907c87F1872458345b3',
-  // '0x46abFE1C972fCa43766d6aD70E1c1Df72F4Bb4d1',
+  '0x46abFE1C972fCa43766d6aD70E1c1Df72F4Bb4d1',
   GnosisSafeL2.abi
 );
